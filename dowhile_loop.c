fbe139548711392b93ac94b71874a3b540a37e8b
Arunbide <112233922+Arunbide@users.noreply.github.com>
2022-11-08 16:20:02 +0530

    Add files via upload

---
+++ dowhile_loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdio.h>
+
+void main()
+{
+	int number;
+	int n;
+   number=1;
+   clrscr();
+
+	printf("Enter the value of N: ");
+	scanf("%d",&n);
+	printf("Odd Numbers from 1 to %d:\n",n);
+ 
+do {
+    if(number%2 != 0)
+	printf("%d ",number);
+		number++;
+     }while(number<=n);
+	 getch();
+}
