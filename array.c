044e3b454219f0e7d5e6e39df1a9610d84af0ab7
arun bide <112233922+Arunbide@users.noreply.github.com>
2022-11-08 23:52:18 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n;
+    double arr[100];
+    clrscr();
+    printf("Enter the number of elements (1 to 100): ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        printf("Enter number%d: ", i + 1);
+        scanf("%lf", &arr[i]);
+    }
+
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[0] < arr[i])
+        {
+            arr[0] = arr[i];
+        }
+    }
+
+    printf("Largest element = %.2lf", arr[0]);
+
+    getch();
+}
