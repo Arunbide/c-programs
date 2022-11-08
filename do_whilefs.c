37469eb9a3cec503884ff7aabc8c37fe112d3200
Arunbide <112233922+Arunbide@users.noreply.github.com>
2022-11-08 16:42:24 +0530

    Add files via upload

---
+++ do_whilefs.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+// #/\include<conio.h>
+ 
+void main()
+{
+    int n,f,f1=-1,f2=1;
+    // clrscr();
+     
+    printf("  Enter The Number Of Terms:");
+    scanf("%d",&n);
+     
+    printf("  The Fibonacci Series is:");
+     
+    do
+    {
+        f=f1+f2;
+        f1=f2;
+        f2=f;
+        printf("  \n %d",f);
+        n--;
+    }while(n>0);
+    // getch();
+}
